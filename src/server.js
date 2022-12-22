e33cfcde35fe6d2c1ee05d76b62929e63310b9be
sZeuSz <rosenosilva@super.ufam.edu.br>
2022-12-22 17:57:32 -0400

    ...

--- src/server.js
+++ src/server.js
@@ -11,6 +11,6 @@ app.use(json());
 app.use(cors());
 app.use(accountsRoute);
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running in port: ${port}`));
